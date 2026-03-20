e5ad1cf3afc455bb2435ddcd4affd69a5fc71a76
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-03-19 23:11:43 -0500

    fix(model): correccion de error por modificar clase Pasajero

--- ArquitecturaEnCapas2/src/main/java/model/PasajeroAdultoMayor.java
+++ ArquitecturaEnCapas2/src/main/java/model/PasajeroAdultoMayor.java
@@ -1,33 +1,38 @@
-
 package model;
 
 /**
  *
  * @author alex
  */
+public class PasajeroAdultoMayor extends Pasajero
+{
 
-public class PasajeroAdultoMayor extends Pasajero {
-
-    public PasajeroAdultoMayor(String cedula, String nombre) {
-        super(cedula, nombre);
+    public PasajeroAdultoMayor(String cedula, String nombre, String fechaNacimiento)
+    {
+        super(cedula, nombre, fechaNacimiento);
     }
 
     @Override
-    public double calcularDescuento() {
+    public double calcularDescuento()
+    {
         return 0.30;
     }
 
     @Override
-    public String getTipo() {
+    public String getTipo()
+    {
         return "AdultoMayor";
     }
 
     @Override
-    public void imprimirDetalle() {
+    public void imprimirDetalle()
+    {
         System.out.println("=== PASAJERO ADULTO MAYOR ===");
         System.out.println("Cedula: " + cedula);
         System.out.println("Nombre: " + nombre);
+        System.out.println("Fecha nacimiento: " + fechaNacimiento);
+        System.out.println("Edad: " + calcularEdad() + " anos");
         System.out.println("Descuento: 30%");
     }
-    
+
 }
