da83821e0c513c61ea84d972a099488d1b06b8e0
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-03-19 23:26:37 -0500

    fix(model): correccion de error en PasajeroRegular por modificar clase Pasajero

--- ArquitecturaEnCapas2/src/main/java/model/PasajeroRegular.java
+++ ArquitecturaEnCapas2/src/main/java/model/PasajeroRegular.java
@@ -1,33 +1,38 @@
-
 package model;
 
 /**
  *
- * @author alex
+ * @authors alex and jose
  */
+public class PasajeroRegular extends Pasajero
+{
 
-public class PasajeroRegular extends Pasajero {
-
-    public PasajeroRegular(String cedula, String nombre) {
-        super(cedula, nombre);
+    public PasajeroRegular(String cedula, String nombre, String fechaNacimiento)
+    {
+        super(cedula, nombre, fechaNacimiento);
     }
 
     @Override
-    public double calcularDescuento() {
-        return 0.0; 
+    public double calcularDescuento()
+    {
+        return 0.0;
     }
 
     @Override
-    public String getTipo() {
-        return "Regular"; 
+    public String getTipo()
+    {
+        return "Regular";
     }
 
     @Override
-    public void imprimirDetalle() {
+    public void imprimirDetalle()
+    {
         System.out.println("=== PASAJERO REGULAR ===");
         System.out.println("Cedula: " + cedula);
         System.out.println("Nombre: " + nombre);
+        System.out.println("Fecha nacimiento: " + fechaNacimiento);
+        System.out.println("Edad: " + calcularEdad() + " anos");
         System.out.println("Descuento: 0%");
     }
-    
+
 }
