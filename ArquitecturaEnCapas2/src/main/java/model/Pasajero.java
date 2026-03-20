973f13dbcab1b60ec159000f0b346f3d980fc1af
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-03-19 23:06:09 -0500

    feat(model): agregar fechaNacimiento y calculo automatico de adulto mayor en la clase Pasajero

--- ArquitecturaEnCapas2/src/main/java/model/Pasajero.java
+++ ArquitecturaEnCapas2/src/main/java/model/Pasajero.java
@@ -1,24 +1,48 @@
-
 package model;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
- * @author alex
+ * @authors alex and jose
  */
+public abstract class Pasajero extends Persona
+{
 
-public abstract class Pasajero extends Persona {
-    
-    public Pasajero(String cedula, String nombre) {
+    protected String fechaNacimiento;
+
+    public Pasajero(String cedula, String nombre, String fechaNacimiento)
+    {
         super(cedula, nombre);
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFechaNacimiento()
+    {
+        return fechaNacimiento;
+    }
+
+    public int calcularEdad()
+    {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate nacimiento = LocalDate.parse(fechaNacimiento, fmt);
+        return Period.between(nacimiento, LocalDate.now()).getYears();
+    }
+
+    public boolean esAdultoMayor()
+    {
+        return calcularEdad() >= 60;
     }
 
     public abstract double calcularDescuento();
+
     public abstract String getTipo();
 
-    public String toTexto() {
-        return getTipo() + ";" + cedula + ";" + nombre;
+    public String toTexto()
+    {
+        return getTipo() + ";" + cedula + ";" + nombre + ";" + fechaNacimiento;
     }
-    
-    
-    
+
 }
