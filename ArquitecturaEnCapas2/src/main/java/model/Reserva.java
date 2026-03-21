521e5b39337f14a6f21f1ca20ed8de57ce239206
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-03-21 10:05:06 -0500

    feat(model): agregando atributos

--- ArquitecturaEnCapas2/src/main/java/model/Reserva.java
+++ ArquitecturaEnCapas2/src/main/java/model/Reserva.java
@@ -1,14 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author Jose
  */
-public class Reserva
+public class Reserva implements Imprimible
 {
-    
+
+    public static final String ACTIVA = "Activa";
+    public static final String CONVERTIDA = "Convertida";
+    public static final String CANCELADA = "Cancelada";
+
+    private String codigo;
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private String fechaCreacion;
+    private String fechaViaje;
+    private String estado;
+    private static final DateTimeFormatter FMT
+            = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
 }
