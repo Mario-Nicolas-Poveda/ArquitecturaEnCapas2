e6e158107cb5249404fa5f33672b61312e633474
Alexander vesga <avesga@unicesar.edu.co>
2026-03-21 11:51:41 -0500

    feat(service): agregar atributos y constante

--- ArquitecturaEnCapas2/src/main/java/service/ReservaService.java
+++ ArquitecturaEnCapas2/src/main/java/service/ReservaService.java
@@ -1,10 +1,23 @@
 
 package service;
 
+import dao.ReservaDAO;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import model.*;
 /**
  *
  * @author alex
  */
 public class ReservaService {
+    private ReservaDAO      reservaDAO;
+    private VehiculoService vehiculoService;
+    private PersonaService  personaService;
+    private TicketService   ticketService;
+    private List<Reserva>   reservas;
+
+    private static final DateTimeFormatter FMT = 
+            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
     
 }
