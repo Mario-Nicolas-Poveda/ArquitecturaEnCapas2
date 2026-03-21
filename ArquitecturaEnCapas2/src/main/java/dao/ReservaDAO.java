97558cbdd1ac24f96b7a9127b4bf12ca1a7e44d6
Alexander vesga <avesga@unicesar.edu.co>
2026-03-21 11:22:05 -0500

    feat(dao): agregar método cargarLineas

--- ArquitecturaEnCapas2/src/main/java/dao/ReservaDAO.java
+++ ArquitecturaEnCapas2/src/main/java/dao/ReservaDAO.java
@@ -3,6 +3,9 @@ package dao;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 import model.Reserva;
 
 /**
@@ -23,4 +26,26 @@ public class ReservaDAO {
             System.out.println("Error al guardar reserva: " + e.getMessage());
         }
     }
+    
+        public List<String[]> cargarLineas() {
+        List<String[]> lista = new ArrayList<>();
+        File f = new File(ARCHIVO);
+        if (!f.exists()) return lista;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(ARCHIVO));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                if (!linea.trim().isEmpty()) {
+                    String[] partes = linea.split(";");
+                    if (partes.length >= 6) lista.add(partes);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer reservas: " + e.getMessage());
+        }
+        return lista;
+    }
+
+        
 }
