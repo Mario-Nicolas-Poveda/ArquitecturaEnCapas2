a4f186a1e5b8d32f3bb749ee02ce5f570122cf71
Alexander vesga <avesga@unicesar.edu.co>
2026-03-21 12:02:54 -0500

    feat(service): agregar método cargarReservas

--- ArquitecturaEnCapas2/src/main/java/service/ReservaService.java
+++ ArquitecturaEnCapas2/src/main/java/service/ReservaService.java
@@ -3,6 +3,7 @@ package service;
 
 import dao.ReservaDAO;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import model.*;
 /**
@@ -19,5 +20,34 @@ public class ReservaService {
     private static final DateTimeFormatter FMT = 
             DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-    
+    public ReservaService(VehiculoService vehiculoService,PersonaService personaService,TicketService ticketService) {
+        this.reservaDAO      = new ReservaDAO();
+        this.vehiculoService = vehiculoService;
+        this.personaService  = personaService;
+        this.ticketService   = ticketService;
+        this.reservas        = new ArrayList<>();
+        cargarReservas();
+        verificarVencidas();
+    }
+
+    private void cargarReservas() {
+        List<String[]> lineas = reservaDAO.cargarLineas();
+        for (String[] p : lineas) {
+            String codigo        = p[0];
+            String cedula        = p[1];
+            String placa         = p[2];
+            String fechaCreacion = p[3];
+            String fechaViaje    = p[4];
+            String estado        = p[5];
+            
+            Pasajero pasajero = personaService.buscarPasajeroPorCedula(cedula);
+            Vehiculo vehiculo = vehiculoService.buscarPorPlaca(placa);
+
+            if (pasajero != null && vehiculo != null) {
+                reservas.add(new Reserva(codigo, pasajero, vehiculo,
+                        fechaCreacion, fechaViaje, estado));
+            }
+        }
+    }
+
 }
