86b9e9680427dc68be500dfbb8677cdc80c96087
Alexander vesga <avesga@unicesar.edu.co>
2026-03-21 11:07:33 -0500

    feat(dao): agregar método guardarReserva

--- ArquitecturaEnCapas2/src/main/java/dao/ReservaDAO.java
+++ ArquitecturaEnCapas2/src/main/java/dao/ReservaDAO.java
@@ -1,6 +1,10 @@
 
 package dao;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import model.Reserva;
+
 /**
  *
  * @author alex
@@ -9,5 +13,14 @@ package dao;
 public class ReservaDAO {
     
         private static final String ARCHIVO = "reservas.txt";
-
+        
+    public void guardarReserva(Reserva r) {
+        try {
+            FileWriter fw = new FileWriter(ARCHIVO, true);
+            fw.write(r.toTexto() + "\n");
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error al guardar reserva: " + e.getMessage());
+        }
+    }
 }
